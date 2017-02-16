f2c315267c6924a9b28f060a7c3f0054502b3482
Shehlath Misri <shehlathmisri@Shehlaths-MacBook-Pro.local>
2017-02-16 17:34:27 +0530

    changes commited

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ app.controller("TaskController", function() {
   vm.newTask = function(){
     console.log('call');
     vm.taskList.toDo.push({
+      id: guid(),
       name: vm.name,
       desc: vm.desc
     });
@@ -21,8 +22,47 @@ app.controller("TaskController", function() {
 
   };
 
-  vm.moveTask = function(){
+  vm.moveTask = function(type, id){
     console.log('call');
+    var temp;
+
+    if(type==='todo'){
+      for(var i = 0; i < vm.taskList.toDo.length; i++){
+        var obj = vm.taskList.toDo[i];
+
+        if(obj.id === id)
+        {
+          temp = vm.taskList.toDo.splice(i,1);
+        }
+      }
+      vm.taskList.inProgress.push(temp[0]);
+
+    }
+
+    if(type==='inProgress'){
+      for(var i = 0; i < vm.taskList.inProgress.length; i++){
+        var obj = vm.taskList.inProgress[i];
+
+        if(obj.id === id)
+        {
+          temp = vm.taskList.inProgress.splice(i,1);
+        }
+      }
+      vm.taskList.done.push(temp[0]);
+
+    }
+
+
   };
+
+  function guid() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+  }
 
 });
