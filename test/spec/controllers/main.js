c5a67431b938f8e2967378f131c4e23019a786f0
Shehlath Misri <shehlathmisri@Shehlaths-MacBook-Pro.local>
2017-02-17 00:25:53 +0530

    test case added

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('Controller: TaskController', function () {
 
   // load the controller's module
-  beforeEach(module('scrumBoardApp'));
+  beforeEach(module('app'));
 
   var MainCtrl,
     scope;
@@ -11,13 +11,13 @@ describe('Controller: MainCtrl', function () {
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
+    MainCtrl = $controller('TaskController', {
       $scope: scope
       // place here mocked dependencies
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(MainCtrl.awesomeThings.length).toBe(3);
+  it('controller should be defined', function () {
+    expect(MainCtrl).toBeDefined();
   });
 });
